5970cc9d3d1d0840c09f7a61681ab58b00e5905f
David <sanchez.dav90@gmail.com>
2019-08-22 23:56:35 -0500

    Add debug file

--- lib/bigbang.rb
+++ lib/bigbang.rb
@@ -1,6 +1,12 @@
 require "bigbang/version"
+require "bigbang/array"
 
 module Bigbang
-  class Error < StandardError; end
-  # Your code goes here...
+  class Application
+    def call(env)
+      `echo debug > debug.txt`;
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from big bang framework!"]]
+    end
+  end
 end
